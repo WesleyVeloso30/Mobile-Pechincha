fb10e1ef6af160b88d4f3065fc6deb13f2327015
Wesley <wesleyvc2110@gmail.com>
2023-03-15 17:34:42 -0300

    add state and props

---
+++ components/Button.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function Button2({name, onClick}) {
+  const styles = StyleSheet.create({
+    Button: {
+      marginBottom: 10,
+      width: 300,
+      height: 40,
+      borderRadius: 20,
+      fontSize: 25,
+      backgroundColor: 'blue',
+      textAlign: 'center',
+      color: 'white',
+      
+    },
+    container2: {
+      // flex: 1,
+      inset: 0,
+      justifyContent: 'space-around',
+      
+    }
+  });
+
+  
+
+  return (
+    <View style={styles.container2} onClick={() => onClick((state) => !state)}>
+      <Text style={styles.Button}>
+        {name}
+      </Text>
+    </View>
+  );
+};
