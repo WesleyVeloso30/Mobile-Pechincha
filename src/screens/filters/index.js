62173bfb26e50e490964cf217ac73c3347d3e82e
Wesley <wesleyvc2110@gmail.com>
2023-09-03 15:05:11 -0300

    feat: init filter and getCompany

--- src/screens/filters/index.js
+++ src/screens/filters/index.js
@@ -1,9 +1,58 @@
 import { View, Text, ScrollView, TextInput } from "react-native";
 import styles from "./styles";
-import { useState } from "react";
+import company from "../../services/company";
+import products from "../../services/product";
+import { useEffect, useState } from "react";
+
+const productService = new products();
+const companyService = new companys();
 
 const Filters = () => {
     const { companysName, setCompanysName } = useState(null);
+    const { productTitles, setProductTitles } = useState(null);
+    useEffect(() => {
+        getCompany();
+    }, []);
+    
+    const getCompany = async () => {
+        let companyNames;
+        setCompanysName(null);
+        if (isMocked) {
+          companyNames = [
+            {
+                name: 'R Carvalho',
+            },
+            {
+                name: 'Assaí',
+            },
+            {
+                name: 'Atacadão',
+            },
+          ];
+          setCompanysName(companyNames);
+          productTitles = [
+            {
+                title: 'Kg de Linguiça',
+            },
+            {
+                titles: 'Carne na Rola',
+            },
+            {
+                titles: 'Sabonete Ypê',
+            },
+          ];
+          setProductTitles(productTitles);
+        } else {
+          setRefreshing(true);
+          companyNames = await companyService.getProducts();
+          companyNames = await productService.getProducts();
+    
+          setCompanysName(companyNames);
+        }
+        setRefreshing(false);
+        return;
+    }
+
     return (
         <ScrollView>
             <TextInput></TextInput>
---
+++ src/services/company.js
@@ -0,0 +1,21 @@
+import Constants from 'expo-constants';
+
+const { baseUrl } = Constants.manifest.extra;
+
+export default class Company {
+  async getCompanys() {
+    try {
+      const resp = await fetch(`${baseUrl}/company`);
+      if (resp.status == 200) {
+        const resposta = await resp.json();
+        return resposta;
+      } else {
+        console.log("Erro: ", resp);
+        alert("Erro ao consultar as companias");
+      }
+    } catch (error) {
+      console.log("Erro: ", resp);
+      alert("Erro ao consultar as companias");
+    }
+  }
+}
