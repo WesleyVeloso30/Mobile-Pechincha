5f21a5f586dbc2049260d91644f5718a485b3ea6
Wesley <wesleyvc2110@gmail.com>
2023-10-24 22:35:39 -0300

    add getTitles service and uncommenting company ser

--- src/services/company.js
+++ src/services/company.js
@@ -1,21 +1,23 @@
-// import Constants from 'expo-constants';
+import Constants from "expo-constants";
 
-// const { baseUrl } = Constants.manifest.extra;
+const { baseUrl } = Constants.manifest.extra;
 
-// export default class Company {
-//   async getCompanys() {
-//     try {
-//       const resp = await fetch(`${baseUrl}/company`);
-//       if (resp.status == 200) {
-//         const resposta = await resp.json();
-//         return resposta;
-//       } else {
-//         console.log("Erro: ", resp);
-//         alert("Erro ao consultar as companias");
-//       }
-//     } catch (error) {
-//       console.log("Erro: ", resp);
-//       alert("Erro ao consultar as companias");
-//     }
-//   }
-// }
+export default class Company {
+  async getCompanys() {
+    try {
+      const resp = await fetch(`${baseUrl}/company`);
+      if (resp.status == 200) {
+        const resposta = await resp.json();
+        return resposta;
+      } else {
+        console.log("Erro: ", resp);
+        alert("Erro ao consultar as companias");
+        return "erro";
+      }
+    } catch (error) {
+      console.log("Erro: ", resp);
+      alert("Erro ao consultar as companias");
+      return "erro";
+    }
+  }
+}
--- src/services/product.js
+++ src/services/product.js
@@ -1,4 +1,4 @@
-import Constants from 'expo-constants';
+import Constants from "expo-constants";
 
 const { baseUrl } = Constants.manifest.extra;
 
@@ -32,6 +32,26 @@ export default class Product {
     } catch (error) {
       console.log("Erro: ", error);
       alert("Erro ao consultar api");
+    }
+  }
+
+  async getTitles() {
+    try {
+      const resp = await fetch(
+        `${baseUrl}/product/titles?dateLimit=2021-01-12T00:00:00.000`
+      );
+      if (resp.status == 200) {
+        const resposta = await resp.json();
+        return resposta;
+      } else {
+        console.log("Erro: ", resp);
+        alert("Erro ao consultar titulos do produto");
+        return "erro";
+      }
+    } catch (error) {
+      console.log("Erro: ", error);
+      alert("Erro ao consultar titulos do produto");
+      return "erro";
     }
   }
 }
